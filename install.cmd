146a74fc5883931a72112986c3b48b607544b30c
intersystems-au <55474783+intersystems-au@users.noreply.github.com>
2019-12-30 11:34:08 +1100

    Changed install to point to folder
    Rather than copy the files to janban folder... use the current location. Enables you to just use git pull to update

--- install.cmd
+++ install.cmd
@@ -1,16 +1,15 @@
 @echo off
 echo ==========================================================
-if not exist %USERPROFILE%\janban (mkdir %USERPROFILE%\janban)
 if not exist kanban.html (
       echo == The install script is running in the wrong folder
+      echo == run this from the folder containing the kanban.html file
       pause
       goto :FINI
 )
-
-echo == Copying files to %USERPROFILE%\janban...
-robocopy /mir . %USERPROFILE%\janban > nul 
-echo == Complete.
-
+echo ==
+echo == configuring outlook to use the following location:
+echo == %cd%\kanban.html
+echo ==
 set offver=0
 for /l %%x in (12, 1, 19) do (
       reg export HKCU\Software\Microsoft\Office\%%x.0\Outlook\Today %%x /y > nul 2> nul
@@ -32,7 +31,7 @@ if %offver%==0 (
 if not %offver%==0 (
 	  set offver=
       reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 1 /f  > nul 2> nul
-      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d %USERPROFILE%\janban\kanban.html /f  > nul 2> nul
+      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d %cd%\kanban.html /f  > nul 2> nul
 	  echo == The install script has completed successfully. Enjoy
       goto :FINI
 )
